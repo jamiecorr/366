8b9713878bf96a14fb4bd314813d364f951b5c99
Samuel Chase <samuelbchase@gmail.com>
2018-01-26 15:00:01 -0800

    Create process_data.sql
    File which will turn the csv files into a functional database

---
+++ process_data.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS cpe366.CP_Account;
+DROP TABLE IF EXISTS cpe366.CP_Device;
+DROP TABLE IF EXISTS cpe366.CP_Device_Model;
+DROP TABLE IF EXISTS cpe366.CP_Email_Final;
+
+
+CREATE TABLE cpe366.CP_Account LIKE cpe366_readonly.CP_Account;
+INSERT cpe366.CP_Account SELECT * FROM cpe366_readonly.CP_Account;
+
+CREATE TABLE cpe366.CP_Device LIKE cpe366_readonly.CP_Device;
+INSERT cpe366.CP_Device SELECT * FROM cpe366_readonly.CP_Device;
+
+CREATE TABLE cpe366.CP_Device_Model LIKE cpe366_readonly.CP_Device_Model;
+INSERT cpe366.CP_Device_Model SELECT * FROM cpe366_readonly.CP_Device_Model;
+
+CREATE TABLE cpe366.CP_Email_Final LIKE cpe366_readonly.CP_Email_Final;
+INSERT cpe366.CP_Email_Final SELECT * FROM cpe366_readonly.CP_Email_Final;

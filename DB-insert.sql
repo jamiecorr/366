0ba515b4606bbeb1dfa087b0d0e541bc43926b23
JCorr <jccorr@calpoly.edu>
2018-03-07 11:56:08 -0800

    Update DB-insert.sql

--- DB-insert.sql
+++ DB-insert.sql
@@ -1,13 +1,13 @@
-CREATE TABLE CP_Account LIKE cpe366_readonly.CP_Account;
+CREATE TABLE IF NOT EXISTS CP_Account LIKE cpe366_readonly.CP_Account;
 INSERT CP_Account SELECT * FROM cpe366_readonly.CP_Account;
 
-CREATE TABLE CP_Device LIKE cpe366_readonly.CP_Device;
+CREATE TABLE IF NOT EXISTS CP_Device LIKE cpe366_readonly.CP_Device;
 INSERT CP_Device SELECT * FROM cpe366_readonly.CP_Device;
 
-CREATE TABLE CP_Device_Model LIKE cpe366_readonly.CP_Device_Model;
+CREATE TABLE IF NOT EXISTS CP_Device_Model LIKE cpe366_readonly.CP_Device_Model;
 INSERT CP_Device_Model SELECT * FROM cpe366_readonly.CP_Device_Model;
 
-CREATE TABLE CP_Email_Final LIKE cpe366_readonly.CP_Email_Final;
+CREATE TABLE IF NOT EXISTS CP_Email_Final LIKE cpe366_readonly.CP_Email_Final;
 INSERT CP_Email_Final SELECT * FROM cpe366_readonly.CP_Email_Final;
 
 
